3fac1403f3a273f5ea1de0b22f380a393be1651d
Diwakar <diwakarchauhaun827@gmail.com>
2023-09-06 15:21:24 +0530

    final

--- book-store/src/components/Book/Books.js
+++ book-store/src/components/Book/Books.js
@@ -1,36 +1,29 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import Book from './Book';
-import './Book.css';
-const URL="http://localhost:5000/books";
-
-const fetchHandler=async()=>{
-  return await axios.get(URL).then((res)=>res.data)
-
-}
+import React, { useEffect, useState } from "react";
+import "./Book.css";
+import axios from "axios";
+import Book from "./Book";
+const URL = "http://localhost:5000/books";
+const fetchHandler = async () => {
+  return await axios.get(URL).then((res) => res.data);
+};
 const Books = () => {
-    const [books,setbooks]=useState();
-    useEffect(()=>{
-
-       fetchHandler().then((data)=>{setbooks(data.books)})
-    },[]);
-    console.log(books);
-    return (
-        <div>
-        <ul>
-               {books && books.map((book,i)=>( 
-                   <li className='book' key={i}>
-                      <Book   book={book}></Book>
-
-
-                   </li>
-
-               ))}
-
-        </ul>
-            
-        </div>
-    );
+  const [books, setBooks] = useState();
+  useEffect(() => {
+    fetchHandler().then((data) => setBooks(data.books));
+  }, []);
+  console.log(books);
+  return (
+    <div>
+      <ul>
+        {books &&
+          books.map((book, i) => (
+            <li key={i}>
+              <Book book={book} />
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Books;
--- book-store/src/components/Home.js
+++ book-store/src/components/Home.js
@@ -1,38 +1,22 @@
-import React from 'react';
-import axios from 'axios';
-import { useState,useEffect } from 'react';
-import './home.css';
-import Book from './Book/Book';
-import './Book/Book.css';
-const URL="http://localhost:5000/books";
+import { Button, Typography, Box } from "@mui/material";
+import React from "react";
+import { Link } from "react-router-dom";
 
-const fetchHandler=async()=>{
-  return await axios.get(URL).then((res)=>res.data)
-
-}
 const Home = () => {
-    const [books,setbooks]=useState();
-    useEffect(()=>{
-
-       fetchHandler().then((data)=>{setbooks(data.books)})
-    },[]);
-    console.log(books);
-    return (
-        <div>
-        <ul>
-               {books && books.map((book,i)=>( 
-                   <li className='book' key={i}>
-                      <Book   book={book}></Book>
-
-
-                   </li>
-
-               ))}
-
-        </ul>
-            
-        </div>
-    );
+  return (
+    <div>
+      <Box display="flex" flexDirection="column" alignItems="center">
+        <Button
+          LinkComponent={Link}
+          to="/books"
+          sx={{ marginTop: 15, background: "orangered" }}
+          variant="contained"
+        >
+          <Typography variant="h3">View All products</Typography>
+        </Button>
+      </Box>
+    </div>
+  );
 };
 
 export default Home;
